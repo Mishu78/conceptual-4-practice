da75a2a87c4d582adcfd10c0124a5b27da0de9cb
Mishu78 <meherun2002@gmail.com>
2026-02-22 15:43:58 +0600

    creating blank array and putting interview and reject list

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let interviewLst=[];
 
+// we are putting the list of interview information when interview button is clicked in blank array 
+let interviewList=[];
+let rejectedList=[];
 let total= document.getElementById('total');
 let interviewCount=document.getElementById('interviewCount');
 let rejectedCount=document.getElementById('rejectedCount');
 //console.log(rejectedCount);
 
 const allCardSection=document.getElementById("allCards")
+
+const mainContainer=document.querySelector('main');
+
 function calculateCount(){
     total.innerHTML=allCardSection.children.length;
+    interviewCount.innerText=interviewList.length;
+    rejectedCount.innerHTML=rejectedList.length;
 }
 calculateCount();
+
+function toggleStyle(){
+    console.log("click")
+}
