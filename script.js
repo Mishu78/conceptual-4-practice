7d8a0f40ba6b6f781979972a9cb2b06b0e8263f5
programming hero <meherun2002@gmail.com>
2026-02-22 23:29:23 +0600

    interview button added

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const rejectedFilterBtn=document.getElementById('rejected-filter-btn');
 const allCardSection=document.getElementById("allCards")
 
 const mainContainer=document.querySelector('main');
+const filteredSection=document.getElementById('filtered-section');
 
 // const allFilterBtn=document.getElementById('all-filter-btn').addEventListener('click',function(){
 //     alert('clicked from add event ')
@@ -35,5 +36,42 @@ function toggleStyle(id){
    //adding blue for current button
    const selected=document.getElementById(id);
    selected.classList.add('bg-blue-600','text-white');
+}
+
+mainContainer.addEventListener('click',function(event){
+ if(event.target.classList.contains)('interview-btn'){
+     const parenNode=event.target.parentNode.parentNode;
+ const companyName=parenNode.querySelector('.company-name').innerText;
+ const companyPosition=parenNode.querySelector('.company-position').innerText;
+ const companyType=parenNode.querySelector('.company-type').innerText;
+ const status=parenNode.querySelector('.status').innerText;
+ const notes=parenNode.querySelector('.notes').innerText;
+
+// console.log(companyName,companyPosition,companyType,status,notes);
+
+const cardInfo={
+    companyName,
+    companyPosition,
+    companyType,
+    status,
+    notes,
+}
+//job already exist or not
+const jobExist = interviewList.find(item => item.companyName == cardInfo.companyName);
+if(!jobExist){
+    interviewList.push(cardInfo);
+}
+renderInterview();
+//console.log(interviewList)
+ }
+
+})
+
+function renderInterview(){
+filteredSection.innerHTML=''
+for(let interview of interviewList){
+    let div=document.createElement('div');
+    div.className= 'card flex flex-row justify-between mb-28'
+}
 }
 
